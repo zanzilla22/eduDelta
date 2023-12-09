ce61a64a985593a1cddd277218911ddca2f1f3b6
zanzilla22 <69683242+zanzilla22@users.noreply.github.com>
2023-12-09 12:58:56 -0500

    sum test asker stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
 import openai
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -9,18 +9,31 @@ openai.api_key = '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'
 def index():
     return "Hello, prepare to learn with Mr. Zane!"
 
+@app.route('/testAsk')
+def ask():
+    return render_template("asker.html")
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.json.get('message', '')
 
+    # Print the user input for debugging
+    print(f"Received message: {user_input}")
+
+    # Make the OpenAI API call
     response = openai.Completion.create(
-        model="asst_k1bMHyY9Vo0RO7sVZqWRXVg7", 
+        model="asst_k1bMHyY9Vo0RO7sVZqWRXVg7",
         prompt=user_input,
         max_tokens=150
     )
 
+    # Extract the text response from the OpenAI API response
     text_response = response.choices[0].text.strip() if response.choices else "No response generated."
 
+    # Print the OpenAI response for debugging
+    print(f"Generated response: {text_response}")
+
+    # Return the response to the user
     return jsonify({"response": text_response})
 
 if __name__ == '__main__':
