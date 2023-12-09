81c8ecdc4d9e97136abd14e42c6e0ea4327fe386
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-12-09 16:00:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+// App.js
 
-const Live2DAvatar = ({ modelPath }) => {
-  const canvasRef = useRef(null);
+import React from 'react';
+import Live2DModel from './Live2DModel'; // Import your Live2D component
 
-  useEffect(() => {
-    // Ensure the Live2D SDK is loaded
-    if (window.Live2D) {
-      try {
-        // Initialize the Live2D model here using the SDK
-        // You will need to refer to the SDK documentation for specific initialization code
-        // This typically involves creating a new Live2D model instance
-        // and loading the model data and textures.
-        
-        const live2DModel = new window.Live2DModel(canvasRef.current, modelPath);
-        // Start rendering the model
-        live2DModel.start();
-      } catch (e) {
-        console.error('Error initializing Live2D model:', e);
-      }
-    }
-  }, [modelPath]);
-
+const App = () => {
   return (
-    <canvas ref={canvasRef} width="300" height="500">
-      {/* Canvas where the Live2D model will render */}
-    </canvas>
+    <div>
+      {/* Other components and content */}
+      <Live2DModel /> {/* Render the Live2D component */}
+    </div>
   );
 };
 
-export default Live2DAvatar;
+export default App;
